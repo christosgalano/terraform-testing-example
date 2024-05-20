16e3fc07965eaf65bbc9cd418e215152feba74e4
Christos Galanopoulos <christosgalanop@gmail.com>
2024-05-20 11:23:21 +0300

    Update content

--- terraform.tf
+++ terraform.tf
@@ -7,6 +7,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-christos-galanopoulos"
+    storage_account_name = "stterraformstategalano"
+    container_name       = "terraform-testing-example"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
